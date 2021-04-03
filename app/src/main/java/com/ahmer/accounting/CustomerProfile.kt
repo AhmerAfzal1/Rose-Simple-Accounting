92fb0f52d89890845901d2aa258f927d5b9176fb
Ahmer Afzal <ahmerafzal@yahoo.com>
2021-04-03 14:24:47 +0500

    Mistakenly deleted while reformate code

--- app/src/main/java/com/ahmer/accounting/CustomerProfile.kt
+++ app/src/main/java/com/ahmer/accounting/CustomerProfile.kt
@@ -3,20 +3,56 @@ package com.ahmer.accounting
 class CustomerProfile {
 
     var id: Int = 0
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var name: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var gender: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var address: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var city: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var phone1: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var phone2: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var phone3: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var comment: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 }
